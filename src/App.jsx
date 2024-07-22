89e5c4bcf1531614c20b10cf8d70cde0f64c4e5c
Nahuel Teruel <teruelnahuel10@gmail.com>
2024-07-22 19:34:39 -0300

    sección skills

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import AboutMe from './components/aboutMe/AboutMe'
 import Header from './components/header/Header'
 import Home from './components/home/Home'
+import Skills from './components/skills/Skills'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Header/>
       <Home/>
       <AboutMe/>
+      <Skills/>
     </>
   )
 }
---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,64 @@
+function Skills() {
+
+    return (
+        <section className="container skills">
+            <h2 className='fw-bold fs-1 text-secondary'> Habilidades<span className='text-primary'>.</span></h2>
+            <div className="row row-cols-3 row-cols-md-2 g-3">
+                <div className="col-md-3">
+                    <div className="card card-no-border">
+                        <div className="card-body">
+                            <ul className="list-unstyled">
+                                <li className="text-secondary fs-4 fw-bold">Diseño Web</li>
+                                <li className="fs-5">UI/UX Design</li>
+                                <li className="fs-5">Responsive Design</li>
+                                <li className="fs-5">Wireframing</li>
+                                <li className="fs-5">User Research</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-3">
+                    <div className="card card-no-border">
+                        <div className="card-body">
+                            <ul className="list-unstyled">
+                                <li className="text-secondary fs-4 fw-bold">Frontend</li>
+                                <li className="fs-5">Javascript</li>
+                                <li className="fs-5">ReactJS</li>
+                                <li className="fs-5">NextJS</li>
+                                <li className="fs-5">CSS3</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-3">
+                    <div className="card card-no-border">
+                        <div className="card-body">
+                            <ul className="list-unstyled">
+                                <li className="text-secondary fs-4 fw-bold">Backend</li>
+                                <li className="fs-5">NodeJs</li>
+                                <li className="fs-5">MondoDB</li>
+                                <li className="fs-5">ExpressJS</li>
+                                <li className="fs-5">Vercel</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-3">
+                    <div className="card card-no-border">
+                        <div className="card-body">
+                            <ul className="list-unstyled">
+                                <li className="text-secondary fs-4 fw-bold">Habilidades Blandas</li>
+                                <li className="fs-5">Comunicación efectiva</li>
+                                <li className="fs-5">Colaboración</li>
+                                <li className="fs-5">Proactividad</li>
+                                <li className="fs-5">Responsable</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+  }
+  
+  export default Skills
