d718fa07cfc522fb5e5fd50707f2048b25425cb8
Nahuel Teruel <teruelnahuel10@gmail.com>
2024-07-31 02:26:35 -0300

    cambio responsive

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
     }, []);
 
     return (
-    <section className="container align-items-center" id="home">
+    <section className="container" id="home">
         <div className="d-flex flex-column flex-md-row align-items-center">
             <div className="text-container" data-aos="fade-down">
                 <p className="fs-4 text">Hola, Soy Nahuel 👋🏻</p>
